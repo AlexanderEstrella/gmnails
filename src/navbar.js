a72d46a5204a137543d94ce219f91755ffece0ff
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-01-13 22:27:22 -0500

    mobile screen configuration for services page

--- src/navbar.js
+++ src/navbar.js
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/fontawesome-free-solid";
 import { Link } from "react-router-dom";
 import { faBars } from "@fortawesome/fontawesome-free-solid";
 
-const HandleClick = () => {
-  const [active, setActive] = useState(false);
-};
 const Navbar = () => {
+  const [unset, setUnset] = useState(false);
   return (
     <nav className="nav-container">
       <nav className="nav-title">
-        <h1>GM Nails</h1>
+        <div
+          className={unset ? "nav-links" : "nav-items"}
+          onScroll={() => setUnset(false)}
+        >
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about">About Us</Link>
+          </div>
+          <div>
+            <Link to="/services">Services</Link>
+          </div>
+          <div>
+            <a href="tel:PHONE_NUM">
+              <FontAwesomeIcon icon={faPhone} />
+              407-953-3540
+            </a>
+          </div>
+        </div>
       </nav>
-      <ul className="nav-items">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-      </ul>
-      <nav className="nav-contact">
-        <a href="tel:PHONE_NUM">
-          <FontAwesomeIcon icon={faPhone} />
-          407-953-3540
-        </a>
-      </nav>
-      <button className="hamburger" onClick={HandleClick}>
-        <FontAwesomeIcon icon="fa-solid fa-bars" />
+      <button className="hamburger" onClick={() => setUnset(!unset)}>
+        {<FontAwesomeIcon icon="fa-solid fa-bars" />}
       </button>
     </nav>
   );

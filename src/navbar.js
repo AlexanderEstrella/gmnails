13ae827daf4f8e19478904e17e55ff51436d841e
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-01-14 19:09:44 -0500

    final edits to mobile

--- src/navbar.js
+++ src/navbar.js
@@ -5,34 +5,28 @@ import { Link } from "react-router-dom";
 import { faBars } from "@fortawesome/fontawesome-free-solid";
 
 const Navbar = () => {
-  const [unset, setUnset] = useState(false);
   return (
     <nav className="nav-container">
       <nav className="nav-title">
-        <div
-          className={unset ? "nav-links" : "nav-items"}
-          onScroll={() => setUnset(false)}
-        >
-          <div>
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <Link to="/about">About Us</Link>
-          </div>
-          <div>
-            <Link to="/services">Services</Link>
-          </div>
-          <div>
-            <a href="tel:PHONE_NUM">
-              <FontAwesomeIcon icon={faPhone} />
-              407-953-3540
-            </a>
-          </div>
+        <h1>GM Nails</h1>
+      </nav>
+      <nav className="nav-body">
+        <div className="Nav-item">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="Nav-item">
+          <Link to="/about">About Us</Link>
+        </div>
+        <div className="Nav-item">
+          <Link to="/services">Services</Link>
+        </div>
+        <div className="Nav-item">
+          <a href="tel:PHONE_NUM">
+            <FontAwesomeIcon icon={faPhone} />
+            407-953-3540
+          </a>
         </div>
       </nav>
-      <button className="hamburger" onClick={() => setUnset(!unset)}>
-        {<FontAwesomeIcon icon="fa-solid fa-bars" />}
-      </button>
     </nav>
   );
 };

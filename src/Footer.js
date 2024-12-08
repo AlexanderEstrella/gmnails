4ad1773677c84f74b99faa033c349977bc9c6188
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2024-12-07 23:20:43 -0500

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="Form-Container">
       <ul className="FbusinessCR">
         <li>
-          <h4 className="FBusinessid"> GM Nails</h4>
+          <h3 className="FBusinessid"> GM Nails</h3>
         </li>
         <li>7335 West Sand Lake Rd</li>
         <li>Orlando, FL 32819</li>
